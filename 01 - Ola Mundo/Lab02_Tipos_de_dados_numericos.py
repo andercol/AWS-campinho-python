048f7da2933f3fee809fa9c2094cf38543242bd2
Anderson Colin <andercol@gmail.com>
2024-03-20 00:19:47 +0000

    Lab3 -Uso do tipo de dados de string

--- 01 - Ola Mundo/Lab02_Tipos_de_dados_numericos.py
+++ 01 - Ola Mundo/Lab02_Tipos_de_dados_numericos.py
@@ -8,3 +8,33 @@ print(myValue)
 print(type(myValue))
 
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+#Exercício 4: apresentação do tipo de dado complex
+
+myValue=5j
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+#Exercício 5: apresentação do tipo de dado bool
+
+myValue=False
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
